815c2cf68692d70c8b2736f140a691592fe4a171
AndresMosquera9005 <andrubjj@gmail.com>
2025-07-18 10:17:28 -0500

    se crea la carpeta app-routing-module.ts

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrl: './login.css'
 })
 export class Login {
+  email: string = '';
+  password: string = '';
+
+  constructor(private router: Router) {}
+  verificarDatos(){
+    var correoElectronico = document.getElementById("email") as HTMLLIElement;
+    var clave = document.getElementById("password") as HTMLLIElement;
+
+    const correo = "andrubjj@gmail.com"
+    const contrasena = "12345678"
+
+    if(this.email == "" && this.password == ""){
+      alert("Por favor, ingrese sus datos");
+    }else if(this.email == correo && this.password == contrasena){
+      this.router.navigate(['/home']);
+    }else {
+      alert("datos incorrectos, intente nuevamente con los datos correctos");
+    }
+  }
+  
 
 }
